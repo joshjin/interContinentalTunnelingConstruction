943e7b12944f513d13b70755e2419ce83321e8a5
jin zexi <joshjin@jins-MacBook-Pro.local>
2017-10-13 15:43:07 -0400

    comment added

--- src/cs131/pa2/YourName/BasicTunnel.java
+++ src/cs131/pa2/YourName/BasicTunnel.java
@@ -7,6 +7,7 @@ import cs131.pa2.Abstract.Vehicle;
 import cs131.pa2.Abstract.Log.Log;
 
 public class BasicTunnel extends Tunnel{
+	// list of vehicles
 	final LinkedList<Vehicle> vehicle_list = new LinkedList<>();
 	int count = 0;
 	// constructor
@@ -18,32 +19,42 @@ public class BasicTunnel extends Tunnel{
 		super(name, log);
 	}
 	
+	// 10/13/2017
 	@Override
 	public synchronized boolean tryToEnterInner(Vehicle vehicle) {
+		// case of 3 vehicles #no enter
 		if (count == 3 && vehicle_list.size() == 3) {
 			return false;
 		}
+		// case of 1 or 2 vehicles # car enter only
 		if ((count < 3 && count > 0) 
 				&& (vehicle_list.size() < 3 && vehicle_list.size() > 0)){
 			Vehicle tmp_vehicle = vehicle_list.peek();
+			// case of sled in the list # no enter
 			if (tmp_vehicle instanceof Sled) {
 				return false;
 			} else {
+				// case of sled trying to enter # no enter
 				if (vehicle instanceof Sled) {
 					return false;
-				} else if (!(vehicle.getDirection().toString()
+				} 
+				// case of different directions # no enter
+				else if (!(vehicle.getDirection().toString()
 						.equals(tmp_vehicle.getDirection().toString()))) {
 					return false;
 				}
 			}
 		}
+		// all other cases # enter!
 		vehicle_list.add(vehicle);
 		++count;
 		return true;
 	}
 
+	// 10/13/2017
 	@Override
 	public synchronized void exitTunnelInner(Vehicle vehicle) {
+		// remove matching vehicle
 		for (Vehicle tmp_vehicle : vehicle_list) {
 			if (tmp_vehicle.equals(vehicle)) {
 				vehicle_list.remove(vehicle);
